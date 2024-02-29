51d38d5a679dcb820256243fa5dc91b30162c4bd
Piotr Ciastko <piotr@ciastko.xyz>
2024-02-29 10:25:15 +0700

    ksztalty

---
+++ src/components/ShapeLine.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue'
+
+defineProps<{
+  letter: string
+}>()
+
+const id = ref(`${Math.random()}`)
+const visible = ref(true)
+
+function onClick() {
+  setTimeout(() => {
+    visible.value = false
+  }, 2400)
+}
+
+const randomMinMax = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min)
+</script>
+
+<template>
+  <svg viewBox="0 0 100 100" v-if="visible">
+    <path
+      :id="id"
+      :d="`M ${randomMinMax(10, 20)} ${randomMinMax(10, 20)} C ${randomMinMax(30, 70)} ${randomMinMax(20, 80)} ${randomMinMax(10, 90)} ${randomMinMax(40, 60)} ${randomMinMax(50, 80)} ${randomMinMax(60, 70)} ${randomMinMax(80, 90)} ${randomMinMax(80, 90)}`"
+      class="fill-transparent stroke-red-500"
+    />
+
+    <text>
+      <textPath class="hover:fill-green-500" :href="`#${id}`" startOffset="0%" @click="onClick">
+        <animate attributeName="startOffset" fill="freeze" from="0%" to="150%" begin="click" dur="3s"></animate>
+
+        {{ letter.repeat(20) }}
+      </textPath>
+    </text>
+  </svg>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
+import ShapeLine from '@/components/ShapeLine.vue'
+
+const word = 'pasozyt'
 </script>
 
 <template>
   <main>
-    <p class="text-3xl font-bold underline">
-      Hello!
-    </p>
+    <div class="h-screen w-screen flex items-center justify-center">
+      <ShapeLine
+        :letter="letter"
+        v-for="letter in word"
+        :key="letter"
+        class="absolute inset-0"
+        :style="{
+          transform: `rotate(${Math.random()*Math.PI*360}deg)`,
+        }"
+      />
+    </div>
   </main>
 </template>
