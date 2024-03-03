ce615fd8a06f47bcf6d0abe99cd179bac3171a4b
Piotr Ciastko <piotr@ciastko.xyz>
2024-03-03 19:20:39 +0700

    remove component

--- src/components/TimeTable.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="absolute grid grid-cols-3 gap-x-3 bg-gray-200 p-1 rounded border border-gray-500 leading-none"
-        :style="timetable.pos">
-        <div v-for="time in timetable.times.flat()" :key="`${timetable.id}-${time}`">
-            <span class="text-sm">{{ time }}</span>
-        </div>
-    </div>
-</template>
